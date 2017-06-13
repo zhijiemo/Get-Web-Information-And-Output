10f8290bce7a9b11ca6162cd8dd05fd3a525614b
zhijiemo <750282510@qq.com>
2017-06-13 22:32:34 +0800

    添加项目文件。

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadButton_Click(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show("Holle,world!");
+            //this.Title = DateTime.Now.ToString("F");
+            //LoadButton.Content = AddressTextBox.Text;
+            //NotifacationListBox.Items.Add(TimeSpan.FromMilliseconds(Environment.TickCount));
+            //new NotificationProvider().DownloadAllNotifications(AddressTextBox.Text);
+            try
+            {
+                    NotifacationListBox.Items.Clear();
+                foreach (var item in new NotificationProvider().DownloadAllNotifications(AddressTextBox.Text))
+                {
+                    //MessageBox.Show(item.ToString());
+                    NotifacationListBox.Items.Add(item);
+                }
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                
+            }
+        }
+
+        private void NotifacationListBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            NotifacationListBox.Items.Add("Item1");
+            NotifacationListBox.Items.Add("Item2");
+            NotifacationListBox.Items.Add("Item3");
+        }
+    }
+}
---
+++ WpfApp1/NotificationProvider.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace WpfApp1
+{
+    public  class Notification
+    {
+
+        public string Name { get; set; }
+
+        public string Url { get; set; }
+
+        public string Date { get; set; }
+
+        public Notification(string name, string url, string date)
+        {
+            Name = name;
+            Url = url;
+            Date = date;
+        }
+
+        public override string ToString()
+        {
+            //return $"[{Date}]{Name}";
+            return string.Format("[{0}]{1}", Name, Date);
+        }
+    }
+
+    public class NotificationProvider
+    {
+
+        private readonly HtmlWeb web = new HtmlWeb();
+
+        public IEnumerable<Notification> DownloadAllNotifications(string url)
+        {
+            var doc = web.Load(url);
+            return FindAllNotifications(doc);
+        }
+
+        public IEnumerable<Notification> FindAllNotifications(HtmlDocument document)
+        {
+            HtmlNode node = document.DocumentNode.SelectSingleNode("//div[@class='list_main_content']");
+            foreach (var i in node.SelectNodes("./div"))
+            {
+                string name = null, url = null, date = null;
+                var namenode = i.SelectSingleNode("./a");
+                name = namenode.GetAttributeValue("title", "");
+                var urlnode = i.SelectSingleNode("./a");
+                url = urlnode.GetAttributeValue("href", "");
+                var datenode = i.SelectSingleNode("./span");
+                date = datenode.InnerText;
+                yield return new Notification(name, url, date);
+            }
+
+        }
+    }
+}
